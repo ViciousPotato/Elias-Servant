90807be66986707de92f64ff93ecea8b64de2c3a
Zhou Honglin <Zhou.Honglin@Gmail.com>
2014-11-22 14:37:00 +0000

    Add Elias auto pulling

--- servant.js
+++ servant.js
@@ -1,6 +1,17 @@
-var express = require('express')
-var app = express
+var express = require('express');
+var app = express();
+var exec = require('child_process').exec;
 
-app.get('/payload', function(req, res) {
-  console.log(req);
+app.post('/elias-github-events/:evt', function(req, res) {
+  console.log("Elias got some commits, updating...");
+  
+  exec("cd ../Elias ; git pull origin master",
+         function(error, stdout, stderr) {
+           console.log(stdout);
+           console.log(stderr);
+         });
+
+  res.send("ok");
 })
+
+app.listen(9000)
